4c1b01375df7b3a81367096ae84698d3cd736e8a
JankiVerloop <jankiverloop@gmail.com>
2026-07-03 14:46:28 +0530

    otp step 2 complete after remarketing msg shoot

--- app/Console/Commands/RemarketingSmsCommand.php
+++ app/Console/Commands/RemarketingSmsCommand.php
@@ -73,6 +73,7 @@ class RemarketingSmsCommand extends Command
         $users = DB::table('customers')
             ->whereDate('created_at', $createdDate)
             ->where('is_paid', 0)
+            ->where('registration_step', '>=', 3)
             ->where('is_active', 1)
             ->where('is_dnd', 0)
             ->whereNull('deleted_at')
