a111e110f2b1ac5d832e15fe4c21413a1cd22098
verloopdev8-lab <verloop.dev8@gmail.com>
2026-09-01 10:45:58 +0530

    remark added

--- database/seeders/ApplicationStatusSeeder.php
+++ database/seeders/ApplicationStatusSeeder.php
@@ -35,21 +35,25 @@ class ApplicationStatusSeeder extends Seeder
 
             ['status_name' => 'Details Verification Pending 3 Days Reject', 'slug' => 'details_verification_pending_3_days_reject', 'priority_no' => 10, 'colorclass' => 'red', 'step' => 16],
 
-            ['status_name' => 'Appointment Scheduled', 'slug' => 'appointment_scheduled', 'priority_no' => 11, 'colorclass' => 'orange', 'step' => 17],
+            ['status_name' => 'Appointment Booked Pending Warning', 'slug' => 'appointment_booked_pending_warning', 'priority_no' => 11, 'colorclass' => 'yellow', 'step' => 17],
 
-            ['status_name' => 'Appointment Rescheduled 1', 'slug' => 'appointment_rescheduled1', 'priority_no' => 12, 'colorclass' => 'orange', 'step' => 18],
+            ['status_name' => 'Appointment Booked Pending Reject', 'slug' => 'appointment_booked_pending_reject', 'priority_no' => 12, 'colorclass' => 'red', 'step' => 18],
 
-            ['status_name' => 'Appointment Rescheduled 2', 'slug' => 'appointment_rescheduled2', 'priority_no' => 13, 'colorclass' => 'orange', 'step' => 19],
+            ['status_name' => 'Appointment Scheduled', 'slug' => 'appointment_scheduled', 'priority_no' => 13, 'colorclass' => 'orange', 'step' => 19],
 
-            ['status_name' => 'Appointment Rescheduled 3', 'slug' => 'appointment_rescheduled3', 'priority_no' => 14, 'colorclass' => 'orange', 'step' => 20],
+            ['status_name' => 'Appointment Rescheduled 1', 'slug' => 'appointment_rescheduled1', 'priority_no' => 14, 'colorclass' => 'orange', 'step' => 20],
 
-            ['status_name' => 'POV Success', 'slug' => 'pov_success', 'priority_no' => 15, 'colorclass' => 'green', 'step' => 21],
+            ['status_name' => 'Appointment Rescheduled 2', 'slug' => 'appointment_rescheduled2', 'priority_no' => 15, 'colorclass' => 'orange', 'step' => 21],
 
-            ['status_name' => 'POV Failed', 'slug' => 'pov_failed', 'priority_no' => 16, 'colorclass' => 'red', 'step' => 22],
+            ['status_name' => 'Appointment Rescheduled 3', 'slug' => 'appointment_rescheduled3', 'priority_no' => 16, 'colorclass' => 'orange', 'step' => 22],
 
-            ['status_name' => 'POV Insufficient Documents', 'slug' => 'pov_insufficient_documents', 'priority_no' => 17, 'colorclass' => 'pink', 'step' => 23],
+            ['status_name' => 'POV Success', 'slug' => 'pov_success', 'priority_no' => 17, 'colorclass' => 'green', 'step' => 23],
 
-            ['status_name' => 'Not Intersted', 'slug' => 'not_intersted', 'priority_no' => 18, 'colorclass' => 'red', 'step' => 24],
+            ['status_name' => 'POV Failed', 'slug' => 'pov_failed', 'priority_no' => 18, 'colorclass' => 'red', 'step' => 24],
+
+            ['status_name' => 'POV Insufficient Documents', 'slug' => 'pov_insufficient_documents', 'priority_no' => 19, 'colorclass' => 'pink', 'step' => 25],
+
+            ['status_name' => 'Not Intersted', 'slug' => 'not_intersted', 'priority_no' => 20, 'colorclass' => 'red', 'step' => 26],
         ];
 
         foreach ($statuses as $status) {
